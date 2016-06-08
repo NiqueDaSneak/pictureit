01c3f76ad4542191e03ef7aefa15e2d292caaf72
niquedasneak <niquedasneak4@gmail.com>
2016-06-08 10:49:58 -0400

    add route for landing page for artists as well as post route for form submission and redirection upon successful posting

--- routes/artists.js
+++ routes/artists.js
@@ -1,14 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var Image = require('./../models/image.js')
+
 
 // Homepage for website
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PictureIt' });
+	res.render('index', { title: 'PictureIt' });
 });
 
 // for images being submitted
 router.post('/submit-image', function(req, res, next) {
-  console.log('The price of this piece is $' + req.body.price)
+	var newImage = new Image({
+		imageData: "test for now",
+		artistEmail: req.body.artistEmail,
+		price: req.body.price,
+		startDate: req._startTime,
+		oneOfOne: checkIfOneOfOne(req)
+	});
+	console.log(newImage + " " + newImage.price)
+
+	newImage.save(function(err, newImage){
+		if (err) {
+			console.log("New Image not saved!")
+			console.log(err);
+			res.redirect('/');
+		} else {
+			console.log("New Image saved!")
+			res.redirect('/submission-success');
+		}
+	});
+
 });
+
+router.get('/submission-success', function(req, res, next) {
+	res.render('successful', { title: 'Successful' });
+});
+
+function checkIfOneOfOne(req) {
+	if (req.body.oneOfOne === 'on') {
+		return true
+	} else {
+		return false
+	}
+}
 
 module.exports = router;
