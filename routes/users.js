c5930878bc7fa74419aa6fb4ba66ec113d7fc1ea
niquedasneak <niquedasneak4@gmail.com>
2016-06-14 16:40:44 -0400

    remove gcloud dependecy

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var gcloud = require('gcloud')({
-	keyFilename: '/path/to/keyfile.json',
-	projectId: 'grape-spaceship-123'
-});
-
-var vision = gcloud.vision();
 
 // Facebook Messenger Webhook setup
 
