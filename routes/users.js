f19ce0974a261b1b6380e9c136d8e4acd8d91d96
niquedasneak <niquedasneak4@gmail.com>
2016-06-13 21:55:59 -0400

    add gcloud npm package

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var gcloud = require('gcloud')({
+  keyFilename: '/path/to/keyfile.json',
+  projectId: 'grape-spaceship-123'
+});
+
+var vision = gcloud.vision();
 
 // Facebook Messenger Webhook setup
 router.get('/', function(req, res, next) {
