dd977aaa24a2497c2b0fc1205003caf7c4e9f080
niquedasneak <niquedasneak4@gmail.com>
2016-06-14 15:49:54 -0400

    begin facebook webhook integration

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var gcloud = require('gcloud')({
-  keyFilename: '/path/to/keyfile.json',
-  projectId: 'grape-spaceship-123'
+	keyFilename: '/path/to/keyfile.json',
+	projectId: 'grape-spaceship-123'
 });
 
 var vision = gcloud.vision();
 
 // Facebook Messenger Webhook setup
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var verify_token = ;
+var token = ;
+
+router.get('/webhook/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+
+  if (req.query['hub.verify_token'] === verify_token) {
+  	res.send(req.query['hub.challenge']);
+  }
+
+  res.send('Error, wrong validation token');
+  
 });
 
 module.exports = router;
