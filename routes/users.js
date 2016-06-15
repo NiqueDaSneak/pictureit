bb947c78f0ad8c41b953a95a6ce5795afb7e4f57
niquedasneak <niquedasneak4@gmail.com>
2016-06-15 10:11:17 -0400

    change app.post to router.post

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.get('/webhook', function(req, res, next) {
 
 });
 
-app.post('/webhook', function (req, res) {
+router.post('/webhook', function (req, res) {
 
     var messaging_events = req.body.entry[0].messaging;
 
