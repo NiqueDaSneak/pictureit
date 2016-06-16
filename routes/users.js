67dea3890b0ed15580ab4e62af7297e783c207ab
niquedasneak <niquedasneak4@gmail.com>
2016-06-16 15:08:47 -0400

    only function should be responding with entry textq

--- routes/users.js
+++ routes/users.js
@@ -13,67 +13,67 @@ var verify_token = "14fc650c879aa056c98b";
 var token = "EAABbvdDRlgIBAFO7ZAl40qkyOhqdcTHZAgmiAfZACxffZAv0Ot7FEBhWAZAFXnQ71ymGuwyu3JosaGYS4L9s70Q586X2f6HoEDUa3uefUB4xhBeJyBYJ0TS52dxKdemNudZBlyzHlZBj2xKSdiJ7bEIJZCJjlWMZCOuRtTzFqTQPxqwZDZD";  
 
 
-// router.get('/webhook', function(req, res, next) {
-//   if (req.query['hub.verify_token'] === verify_token) {
-//     console.log("Validating webhook");
-//     res.status(200).send(req.query['hub.challenge']);
-//   } else {
-//     console.error("Failed validation. Make sure the validation tokens match.");
-//     res.sendStatus(403);          
-//   }  
-// });    
-
 router.get('/webhook', function(req, res, next) {
+  if (req.query['hub.verify_token'] === verify_token) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});    
 
-	if (req.query['hub.verify_token'] === verify_token) {
-		res.send(req.query['hub.challenge']);
-	}
+// router.get('/webhook', function(req, res, next) {
 
-	res.send('Error, wrong validation token');
+// 	if (req.query['hub.verify_token'] === verify_token) {
+// 		res.send(req.query['hub.challenge']);
+// 	}
 
-});
-
-// router.post('/webhook', function (req, res, next) {
-
-// 	var messaging_events = req.body.entry[0].messaging;
-
-// 	// console.log("This is the messaging_events: " + messaging_events)
-
-// 	for (var i = 0; i < messaging_events.length; i++) {
-
-// 		var event = req.body.entry[0].messaging[i];
-// 		var sender = event.sender.id;
-
-// // console.log('XXXXXXX' + event.message.text + 'XXXXXXX');
-// // console.log('!!!!!!!!!!!!!' + event.message.attachments + '!!!!!!!!!!!!!');
-
-// 		// checking for images sent by user
-// 		if (event.message && event.message.text) {
-// 			sendTextMessage(sender, "Thanks for using PictureIT! If you have some art you want to buy, take a photo of its description card and send it to me!");
-// 		} 
-
-// 		// if (event.postback) {
-// 		// 	sendTextMessage(sender, 'Image recieved');
-// 		// 	console.log("This is the event.postback: "+ event.postback);
-// 		// 	continue
-
-// 		// }
-// 		if (event.message.attachments[0]) {
-// 			if (event.message.attachments[0].type === 'image') {
-// 				var imageURL = event.message.attachments[0].payload.url;
-// 				console.log(imageURL);
-// 				sendTextMessage(sender, 'Image recieved');
-// 			}
-// 		} 
-
-// 		// else {
-// 		// 	sendTextMessage(sender, "Something went wrong");
-// 		// }
-// }
-
-// 	res.sendStatus(200);
+// 	res.send('Error, wrong validation token');
 
 // });
+
+router.post('/webhook', function (req, res, next) {
+
+	var messaging_events = req.body.entry[0].messaging;
+
+	// console.log("This is the messaging_events: " + messaging_events)
+
+	for (var i = 0; i < messaging_events.length; i++) {
+
+		var event = req.body.entry[0].messaging[i];
+		var sender = event.sender.id;
+
+// console.log('XXXXXXX' + event.message.text + 'XXXXXXX');
+// console.log('!!!!!!!!!!!!!' + event.message.attachments + '!!!!!!!!!!!!!');
+
+		// checking for images sent by user
+		if (event.message && event.message.text) {
+			sendTextMessage(sender, "Thanks for using PictureIT! If you have some art you want to buy, take a photo of its description card and send it to me!");
+		} 
+
+		// if (event.postback) {
+		// 	sendTextMessage(sender, 'Image recieved');
+		// 	console.log("This is the event.postback: "+ event.postback);
+		// 	continue
+
+		// }
+		// if (event.message.attachments[0]) {
+		// 	if (event.message.attachments[0].type === 'image') {
+		// 		var imageURL = event.message.attachments[0].payload.url;
+		// 		console.log(imageURL);
+		// 		sendTextMessage(sender, 'Image recieved');
+		// 	}
+		// } 
+
+		// else {
+		// 	sendTextMessage(sender, "Something went wrong");
+		// }
+}
+
+	res.sendStatus(200);
+
+});
 
 function sendTextMessage(sender, text) {
 	var messageData = {
