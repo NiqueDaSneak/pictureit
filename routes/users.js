a7f30e626a3e4bc7fa4d9a5a3c877dae1bf693e7
niquedasneak <niquedasneak4@gmail.com>
2016-06-17 14:36:18 -0400

    test google api output

--- routes/users.js
+++ routes/users.js
@@ -61,7 +61,7 @@ router.post('/webhook', function (req, res, next) {
 					if (err) {
 						console.log(err);
 					} else {
-						console.log('GOOGLE FOUND THESE WORDS: ' + text[0]);
+						console.log('GOOGLE FOUND THESE WORDS: ' + text[-1]);
 					}
 				});
 				continue
